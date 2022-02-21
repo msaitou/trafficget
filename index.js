bd2eeeb05883f7c3d2a53a9d8af158edcc0958a1
msaito <peterpansymdromer@gmail.com>
2022-02-21 16:40:58 +0900

    add apply

---
+++ index.js
@@ -0,0 +1,40 @@
+// DBから取得したデータをファイル名にしてデスクトップに作る。
+const logger = require("./initter.js").log();
+global.log = logger;
+logger.info("start!");
+logger.info(process.argv);
+const db = require("./initter.js").db;
+const fs = require("fs");
+const conf = require("config");
+async function start(mode) {
+  return new Promise(async (resolve, reject) => {
+    // db
+    let recs = await db("life_util", "find", {});
+    // ファイル名を作成
+    for (let i = 0; i < recs.length; i++) {
+      let abFilePath = `${conf.traffic.path}/${recs[i].disp_mess}`;
+      fs.writeFileSync(abFilePath, '');
+    }
+    logger.info(33);
+    resolve(true);
+  })
+    .then((res) => {
+      logger.info("res", res);
+    })
+    .catch((e) => {
+      logger.error(e);
+    })
+    .finally(() => {
+      logger.info("tyokuzen");
+      process.exit();
+    });
+}
+start();
+
+// モードの並列実行が可能か。可能ではない場合、終わりを検知できる必要があり、できない場合、起動タイミングは終了タイミングを予測し猶予する必要がある。
+// プロセスに名前をつけて、チェック可能にし、終了するまで。もしくは、終了させてから実行するような仕組みにしたい。
+
+// また、vaiosaito側では、DBから必要な情報を取得して、デスクトップにファイルを作成するみたいなシェルを作りたい。
+
+// log書くやつとdriverとDBの読み書きは独立クラス
+// logを表示するwebアプリを作る。
---
+++ initter.js
@@ -0,0 +1,70 @@
+const conf = require("config");
+
+exports.db = async function (coll, method, cond = {}, doc) {
+  let log = getLogInstance();
+  log.info(0);
+  const mdb = require("mongodb");
+  //   global.db = db;
+  log.info(1);
+  const dbClient = mdb.MongoClient;
+  log.info(2);
+  try {
+    log.info("conf", conf);
+    let db = await dbClient.connect(`mongodb://${conf.db.host}/`);
+    log.info(3);
+    const dbName = db.db("sm");
+    const collection = dbName.collection(coll);
+    let res;
+    log.info(4);
+    switch (method) {
+      case "find":
+        res = await collection.find(cond).toArray();
+        break;
+      case "findOne":
+        res = await collection.findOne(cond);
+        break;
+      case "update":
+        let cnt = 0;
+        if (cond) {
+          cnt = await collection.find(cond).count();
+        }
+        if (cnt) {
+          res = await collection.updateOne(cond, { $set: doc });
+        } else {
+          // insert
+          res = await collection.insertOne(doc);
+        }
+        break;
+      case "remove":
+      default:
+    }
+    log.info(5);
+    // log.info(lines);
+    db.close();
+    return res;
+  } catch (e) {
+    throw e;
+  }
+};
+function getLogInstance() {
+  return global.log ? global.log : thisLog();
+}
+
+/** ログクラスの初期処理
+ * @returns
+ */
+const thisLog = () => {
+  const log = require("log4js");
+  log.configure({
+    appenders: {
+      out: { type: "stdout" },
+      app: { type: "dateFile", filename: "log/a.log", pattern: ".yyyyMMdd", keepFileExt: true },
+    },
+    categories: { default: { appenders: ["out", "app"], level: "all" } },
+  });
+  const logger = log.getLogger();
+  logger.level = "all";
+  return logger;
+};
+exports.log = thisLog;
+
